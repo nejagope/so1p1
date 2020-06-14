94181b3904ff876ffd2e3ced58c5d17e25cb46f2
Mario Alvarado <oiramalli@gmail.com>
2020-06-14 11:31:43 -0600

    implementación de kill

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { interval, Observable } from 'rxjs';
 import { startWith, switchMap } from "rxjs/operators";
-import { config } from 'process';
 
 
 const server = '192.168.0.112';
@@ -24,16 +23,21 @@ const config = {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  urlProcs = config.prod.base + 'procs';
-  urlCPU = config.prod.base + 'cpu';
-  urlMEMO = config.prod.base + 'memo';
+  urlProcs = config.dev.base + 'procs';
+  urlCPU = config.dev.base + 'cpu';
+  urlMEMO = config.dev.base + 'memo';
+  urlProcsInfo = config.dev.base + 'procsinfo';
+  urlKill = config.dev.base + 'killproc';
+
 
   procesos: string[] = [];
   ejecucion: number = 0;
   suspendido: number = 0;
   detenido: number = 0;
   zombie: number = 0;
+  otro: number = 0;
   total: number = 0;
+  kill: number = 0;
 
   cpuUsed: number = 0;
 
@@ -41,60 +45,79 @@ export class AppComponent {
   cpuMemoUsedMb: number = 0;
 
   constructor(private http: HttpClient) {
-    interval(15000)
-      .pipe(
-        startWith(0),
-        switchMap(() => this.getProcs())
-      )
-      .subscribe(res => {
-        this.suspendido = 0;
-        this.procesos.length = 0;
-        for (let key in res) {
-          if (res.hasOwnProperty(key)) {
-            this.procesos.push(res[key]);
-            if (res[key].EstadoID === 'S') {
-              this.suspendido++;
-            } else if (res[key].EstadoID === 'I') {
-            }
+    interval(1000).pipe(
+      startWith(0),
+      switchMap(() => this.getProcs())
+    ).subscribe(res => {
+      this.suspendido = 0;
+      this.procesos.length = 0;
+      for (let key in res) {
+        if (res.hasOwnProperty(key)) {
+          this.procesos.push(res[key]);
+        }
+      }
+      this.total = this.procesos.length;
+    });
+
+    interval(1500).pipe(
+      startWith(0),
+      switchMap(() => this.getCPU())
+    ).subscribe(res => {
+      this.cpuUsed = res['Used'];
+    });
+
+    interval(1000).pipe(
+      startWith(0),
+      switchMap(() => this.getMemo())
+    ).subscribe(res => {
+      this.cpuMemoTotalMb = res['TotalMb'];
+      this.cpuMemoUsedMb = res['UsedMb'];
+    });
+
+    interval(1000).pipe(
+      startWith(0),
+      switchMap(() => this.getProcsInfo())
+    ).subscribe(res => {
+      for (let key in res) {
+        if (res.hasOwnProperty(key)) {
+          if (key === 'Runing') {
+            this.ejecucion = res[key];
+          } else if (key === 'Sleeping') {
+            this.suspendido = res[key];
+          } else if (key === 'Stoped') {
+            this.detenido = res[key];
+          } else if (key === 'Zombie') {
+            this.zombie = res[key];
+          } else if (key === 'Other') {
+            this.otro = res[key];
+          } else if (key === 'Total') {
+            this.total = res[key];
           }
         }
-        this.total = this.procesos.length;
-      });
-
-    interval(3000)
-      .pipe(
-        startWith(0),
-        switchMap(() => this.getCPU())
-      )
-      .subscribe(res => {
-        for (let key in res) {
-          this.cpuUsed = res[key];
-        }
-      });
-
-    interval(2000)
-      .pipe(
-        startWith(0),
-        switchMap(() => this.getMemo())
-      )
-      .subscribe(res => {
-        this.cpuMemoTotalMb = res['TotalMb'];
-        this.cpuMemoUsedMb = res['UsedMb'];
-      });
+      }
+    });
   }
 
   getProcs(): Observable<any> {
-    return this.http
-      .get(this.urlProcs);
+    return this.http.get(this.urlProcs);
   }
 
   getCPU(): Observable<any> {
-    return this.http
-      .get(this.urlCPU);
+    return this.http.get(this.urlCPU);
   }
 
   getMemo(): Observable<any> {
-    return this.http
-      .get(this.urlMEMO);
+    return this.http.get(this.urlMEMO);
   }
+
+  getProcsInfo(): Observable<any> {
+    return this.http.get(this.urlProcsInfo);
+  }
+
+  onClickKill(event): void {
+    const pid: string = event.target.attributes.id.nodeValue;
+    console.log(this.urlKill + `?pid=${pid}`);
+    this.http.get(this.urlKill + `?pid=${pid}`).subscribe();
+  }
+
 }
